15eed18ca43ed0a03dd14d8c49cc447b18107aea
Santiago Perez <sperez@atlassian.com>
2020-07-19 21:12:30 +1000

    Add initial forma layout

---
+++ src/main/kotlin/com/rspn/action/TemplateConfiguration.java
@@ -0,0 +1,14 @@
+package com.rspn.action;
+
+import javax.swing.*;
+
+public class TemplateConfiguration {
+    private JRadioButton ticketAndDescriptionButton;
+    private JRadioButton aZ090RadioButton1;
+    private JRadioButton customRadioButton;
+    private JTextField textField1;
+    private JTextField issueSuffixTextField;
+    private JTextField DEV1234FixBugsTextField;
+    private JTextField DEV1234TextField;
+    private JTextField DEV1234TextField1;
+}
