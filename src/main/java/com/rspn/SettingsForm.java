cf2ec1d6e2b5a818286d528b8b0dcaa101cc0599
Santiago Perez <sperez@atlassian.com>
2020-07-21 21:29:56 +1000

    Add action listeners to radio button and run preview button

---
+++ src/main/java/com/rspn/SettingsForm.java
@@ -0,0 +1,69 @@
+package com.rspn;
+
+import javax.swing.*;
+
+public class SettingsForm {
+    private JRadioButton ticketAndDescriptionRadioButton;
+    private JRadioButton prefixTicketAndDescriptionRadioButton;
+    private JRadioButton customRadioButton;
+    private JTextField customRegexTextField;
+    private JTextField issueSuffixTextField;
+    private JTextField branchNameTextFieldPreview;
+    private JTextField resultingCommitMessageTemplatePreview;
+    private ButtonGroup regexButtonGroup;
+    private JPanel rootPanel;
+
+    private JButton runPreviewButton;
+
+    private JLabel errorLabel;
+
+    public JLabel getErrorLabel() {
+        return errorLabel;
+    }
+
+    public JPanel getRootPanel = rootPanel;
+
+    public JButton getRunPreviewButton() {
+        return runPreviewButton;
+    }
+
+    public JRadioButton getTicketAndDescriptionRadioButton() {
+        return ticketAndDescriptionRadioButton;
+    }
+
+    public JRadioButton getPrefixTicketAndDescriptionRadioButton() {
+        return prefixTicketAndDescriptionRadioButton;
+    }
+
+    public JRadioButton getCustomRadioButton() {
+        return customRadioButton;
+    }
+
+    public JTextField getCustomRegexTextField() {
+        return customRegexTextField;
+    }
+
+    public JTextField getIssueSuffixTextField() {
+        return issueSuffixTextField;
+    }
+
+    public JTextField getBranchNameTextFieldPreview() {
+        return branchNameTextFieldPreview;
+    }
+
+    public JTextField getResultingCommitMessageTemplatePreview() {
+        return resultingCommitMessageTemplatePreview;
+    }
+
+    public ButtonGroup getRegexButtonGroup() {
+        return regexButtonGroup;
+    }
+
+    public JPanel getRootPanel() {
+        return rootPanel;
+    }
+
+    public JPanel getGetRootPanel() {
+        return getRootPanel;
+    }
+}
--- src/main/kotlin/com/rspn/settings/VCSCommitMessageTemplateSettings.kt
+++ src/main/kotlin/com/rspn/settings/VCSCommitMessageTemplateSettings.kt
@@ -1,13 +1,53 @@
 package com.rspn.settings
 
 import com.intellij.openapi.options.SearchableConfigurable
-import com.rspn.TemplateConfiguration
+import com.rspn.SettingsForm
+import java.util.regex.PatternSyntaxException
 import javax.swing.JComponent
 
 class VCSCommitMessageTemplateSettings : SearchableConfigurable {
-    private val vcsCommitMessageTemplateSettingsForm = TemplateConfiguration()
+    private val settingsForm = SettingsForm()
 
-    override fun isModified() = true // TODO("Not yet implemented")
+    companion object {
+        const val sampleCommitMessage = "Sample commit message description"
+    }
+
+    override fun createComponent(): JComponent? {
+        settingsForm.prefixTicketAndDescriptionRadioButton.addActionListener {
+            settingsForm.customRegexTextField.isEnabled = false
+        }
+        settingsForm.ticketAndDescriptionRadioButton.addActionListener {
+            settingsForm.customRegexTextField.isEnabled = false
+        }
+        settingsForm.customRadioButton.addActionListener {
+            settingsForm.customRegexTextField.isEnabled = true
+        }
+        settingsForm.runPreviewButton.addActionListener {
+            val selectedRegexButton =
+                    setOf(settingsForm.customRadioButton, settingsForm.ticketAndDescriptionRadioButton, settingsForm.prefixTicketAndDescriptionRadioButton)
+                            .first { it.isSelected }
+            var customRegex: String? = null
+            if (selectedRegexButton.actionCommand == "Custom") {
+                customRegex = settingsForm.customRegexTextField.text
+            }
+            val suffix = settingsForm.issueSuffixTextField.text
+            val sampleBranchName = settingsForm.branchNameTextFieldPreview.text
+            try {
+                val matchResult = Regex(customRegex ?: selectedRegexButton.actionCommand).find(sampleBranchName)
+                val matchedRegexValue = matchResult?.value
+                settingsForm.resultingCommitMessageTemplatePreview.text = "$matchedRegexValue$suffix$sampleCommitMessage"
+            } catch (e: PatternSyntaxException) {
+                settingsForm.errorLabel.text = e.message
+            }
+        }
+        return settingsForm.getRootPanel
+
+    }
+
+    override fun isModified(): Boolean {
+        //TODO
+        return false
+    }
 
     override fun getId() = "vcs-commit-message-template"
 
@@ -15,9 +55,5 @@ class VCSCommitMessageTemplateSettings : SearchableConfigurable {
 
     override fun apply() {
         // TODO("Not yet implemented")
-    }
-
-    override fun createComponent(): JComponent? {
-        return vcsCommitMessageTemplateSettingsForm.getRootPanel
     }
 }
